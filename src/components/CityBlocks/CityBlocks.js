fb5dd741f88bf4aa9cfe1c935c051cb6e4b06f22
Yibang Chen <ychen622@gmail.com>
2018-03-16 18:02:39 -0700

    file structure

---
+++ src/components/CityBlocks/CityBlocks.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const cityBlocks = (props) => {
+
+	return (
+
+	);
+}
+
+export default cityBlocks;
---
+++ src/components/Itinerary/Itinerary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const itinerary = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+}
+
+export default itinerary;
